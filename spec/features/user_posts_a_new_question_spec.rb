ad3ce608fb38d316e58de17464c8de9547f5bb3d
AlexBennett <alexanderbondbennett@gmail.com>
2015-12-14 14:24:18 -0500

    reworded question test

--- spec/features/user_posts_a_new_question_spec.rb
+++ spec/features/user_posts_a_new_question_spec.rb
@@ -5,11 +5,11 @@ feature "posts a new question" do
     visit new_question_path
     expect(page).to have_content("Post a Question")
     fill_in('Name', :with => 'What am I doing?')
-
     click_button('Create Question')
-    expect(page).to have_content("Description can't be blank")
-    click_link("Back")
 
+    expect(page).to have_content("Description can't be blank")
+
+    click_link("Back")
     expect(page).to have_content("Questions")
     expect(page).to have_no_content("What am I doing?")
   end
@@ -19,13 +19,13 @@ feature "posts a new question" do
     expect(page).to have_content("Post a Question")
     fill_in('Name', :with => 'What am I doing?')
     fill_in('Description', :with => 'This is a description of a question.')
-
     click_button('Create Question')
+
     expect(page).to have_no_content("Name can't be blank")
     expect(page).to have_no_content("Description can't be blank")
   end
 
-  scenario "posts a valid question and index page updates" do
+  scenario "posts a valid question and index page updates to display new question" do
     visit new_question_path
     expect(page).to have_content("Post a Question")
     fill_in('Name', :with => 'What am I doing?')
